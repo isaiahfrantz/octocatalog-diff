be1e72be0dc592f91cc17bcaf1382faa8ac74182
Isaiah Frantz <isaiah.frantz@servicenow.com>
2026-01-27 13:11:47 -0800

    Support comma-separated lists for fact-override and enc-override options
    Add split_override_list method to parse comma-separated override values while preserving commas inside JSON arrays and objects. Update fact-override and enc-override options to accept comma-separated lists using explicit parser.on handlers. Add tests for comma-separated parsing and JSON value preservation. Update documentation to describe comma-separated list support.

--- lib/octocatalog-diff/cli/options/fact_override.rb
+++ lib/octocatalog-diff/cli/options/fact_override.rb
@@ -10,19 +10,21 @@ OctocatalogDiff::Cli::Options::Option.newoption(:fact_override) do
     # Set 'fact_override_in' because more processing is needed, once the command line options
     # have been parsed, to make this into the final form 'fact_override'.
     # Avoid Array parsing here so JSON values with commas stay intact.
-    parser.on('--fact-override STRING', 'Override fact globally') do |x|
+    parser.on('--fact-override STRING1[,STRING2[,...]]', 'Override fact globally') do |x|
       options[:to_fact_override_in] ||= []
       options[:from_fact_override_in] ||= []
-      options[:to_fact_override_in] << x
-      options[:from_fact_override_in] << x
+      OctocatalogDiff::Cli::Options.split_override_list(x).each do |item|
+        options[:to_fact_override_in] << item
+        options[:from_fact_override_in] << item
+      end
     end
-    parser.on('--to-fact-override STRING', 'Override fact for the to branch') do |x|
+    parser.on('--to-fact-override STRING1[,STRING2[,...]]', 'Override fact for the to branch') do |x|
       options[:to_fact_override_in] ||= []
-      options[:to_fact_override_in] << x
+      OctocatalogDiff::Cli::Options.split_override_list(x).each { |item| options[:to_fact_override_in] << item }
     end
-    parser.on('--from-fact-override STRING', 'Override fact for the from branch') do |x|
+    parser.on('--from-fact-override STRING1[,STRING2[,...]]', 'Override fact for the from branch') do |x|
       options[:from_fact_override_in] ||= []
-      options[:from_fact_override_in] << x
+      OctocatalogDiff::Cli::Options.split_override_list(x).each { |item| options[:from_fact_override_in] << item }
     end
   end
 end
